6ac11dd40a26fc09638694e75639070a34a71c50
gc48 <gc48@uw.edu>
2021-04-02 11:49:08 -0700

    Arduino I2C slave code added

---
+++ arduino_i2c_slave/slave_receiver_cdh.ino
@@ -0,0 +1,51 @@
+// Wire Slave Receiver
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Receives data as an I2C/TWI slave device
+// Refer to the "Wire Master Writer" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(0x7E);             // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent); // register event
+  Serial.begin(9600);           // start serial for output
+  Serial.println("I2C slave setup");
+
+}
+
+void loop() {
+  delay(100);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  Serial.println("I2C interrupt");
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+}
+
+void requestEvent() {
+  Serial.println("I2C request recved");
+  uint8_t byt = 0x1f;
+  for(int i=0; i<32; i++){
+    Wire.write(byt--);
+  }
+
+
+  
+//  if (ch > 'z')
+//    ch = '0';
+}
